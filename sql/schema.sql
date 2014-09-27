19e54655d63d35a5a65ed9a01c4901c6b1c0c362
flowers for php <isucon@isucon>
2014-09-27 06:49:02 +0000

    version manage sql

---
+++ sql/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `login` varchar(255) NOT NULL UNIQUE,
+  `password_hash` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL
+) DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS `login_log` (
+  `id` bigint NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  `created_at` datetime NOT NULL,
+  `user_id` int,
+  `login` varchar(255) NOT NULL,
+  `ip` varchar(255) NOT NULL,
+  `succeeded` tinyint NOT NULL
+) DEFAULT CHARSET=utf8;
+
+CREATE INDEX idx_user_id ON login_log(user_id);
+CREATE INDEX idx_succeeded_user_id_id ON login_log(succeeded,user_id,id);
+CREATE INDEX idx_id_succeeded_ip on login_log(id,succeeded,ip);
